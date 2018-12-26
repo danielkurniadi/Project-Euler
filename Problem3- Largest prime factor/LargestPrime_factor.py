fe89f3128cc820b013dedd9c3bbad0ce8456c962
iqDF <daniel.thekurniadi@gmail.com>
2018-12-27 01:09:23 +0800

    solve problem 3

--- Problem3- Largest prime factor/LargestPrime_factor.py
+++ Problem3- Largest prime factor/LargestPrime_factor.py
@@ -1,27 +1,24 @@
 import numpy as np
-
-def sqrt_int(n):
-    # bin search
-    start, end = 1, n
-    mid = (1+n)//2
-    while(start<=end):
-        mid = (start+end)//2
-        if (mid-1)*(mid+1)<=n and mid*mid>=n:
-            return mid
-        elif mid*mid<n:
-            start = mid+1
-        else:
-            end = mid-1
-    return mid
+import time
+import math
 
 def solve(n):
-    for i in range(sqrt_ceil(n), 1, -1):
-        #TODO: if all is False, then confirm this is prime.
-        if 1*(i % np.arange(2, sqrt_int(i)) ==0).all():
+    upper_bound = int(math.sqrt(n))+1
+    for i in range(upper_bound, 1, -1):
+        if (n%i ==0) and np.all(checkPrime(i)):
             return i
+    
+def checkPrime(n):
+    if n==2 or n==3 or n==5:
+        return True
+    if n==4:
+        return False
+    upper_bound = int(math.sqrt(n))+1
+    return np.all(1* (n% np.arange(2, upper_bound) !=0))
 
+def test_solution(n, func): 
+    x = func(n)
+    print(x)
 
 if __name__ == '__main__':
-    tests = [2, 3, 4, 5, 16, 20, 36, 139, 1025]
-    for t in tests:
-        print(t, sqrt_int(t))
+    test_solution(600851475143, solve)
