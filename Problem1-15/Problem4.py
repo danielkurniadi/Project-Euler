3f3f2a2e562d96f2ad055362e9f7267a534adb36
iqDF <daniel.thekurniadi@gmail.com>
2018-12-27 22:18:54 +0800

    add explanation to problem 4

--- Problem1-15/Problem4.py
+++ Problem1-15/Problem4.py
@@ -12,19 +12,23 @@ def findLargest_Po3_Palindrome(palindromes, v):
         square_root = int(math.sqrt(p))
 
         if max_divisor>square_root:
-            print(p)
+            print("The largest palindrome which is a product of two 3-digits number: "p)
+            print("The two 3-digit factors are:" max_divisor, p//max_divisor)
             break
 
 if __name__ == '__main__':
-# Logics for numbers in vector v_even:
-    # upperbound: 999,999 <= 999x999 = 998001, hence largest palindrom = 997799
-    # lowerbound_odd: 99,999 <= 317x317
-    # lowerbound_even: 100x100 = 10,000 (no need)
-    v_even = np.arange(317, 997)[::-1]
+""" 
+  Logical thinking for numbers in vector v:
+    - upperbound: 999,999 <= 999x999 = 998001, hence largest palindrom = 997799
+    - lowerbound_odd: 99,999 <= 317x317
+    - lowerbound_even: 100x100 = 10,000 (no need)  
+  The range of number is still hardcoded for factor.n_digit = 3
+"""
+    v = np.arange(317, 997)[::-1]
 
-    # from v_even, create u_even which is a mirror of v_even, then create palindrome numbers
-    u_even = (v_even//100) + (v_even%10 *99) + (v_even% 100); #print(u_even);
-    palindromes = v_even*1000 + u_even
+    # from v, create u which is a mirror of v, then create palindrome numbers
+    u = (v//100) + (v%10 *99) + (v% 100); #print(u);
+    palindromes = v*1000 + u
 
-    findLargest_Po3_Palindrome(palindromes, v_even)
+    findLargest_Po3_Palindrome(palindromes, v)
 
